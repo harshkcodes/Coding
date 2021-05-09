f97571033dd2a0ac8a2cf8df71d9ad909d22f855
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-10 01:07:53 +0530

    Create a3.c

---
+++ Prajjwal/a3.c
@@ -0,0 +1,22 @@
+//Day 1 b//
+
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+a=34.0;
+b=6.34;
+
+
+printf("a+b= %f\n", a+b);
+printf("a-b= %f\n", a-b);
+printf("a/b= %f\n", a/b);
+printf("a*b= %f\n", a*b);
+
+
+    return 0;
+
+
+}
