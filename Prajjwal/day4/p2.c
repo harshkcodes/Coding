03ccf417521bd88789b4c87aea3c77a4837685e9
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-13 16:58:33 +0530

    Create p2.c

---
+++ Prajjwal/day4/p2.c
@@ -0,0 +1,24 @@
+//day 4  2//
+
+#include <stdio.h>
+
+
+ int main()
+ 
+
+ {
+     int i,c=1;
+     printf("Enter the number\t");
+     scanf("%d", &i);
+
+     while (c<=i)
+     {
+        printf("%d\t",c);
+        c++;
+     }
+
+     
+     
+     return 0;
+ }
+ 
