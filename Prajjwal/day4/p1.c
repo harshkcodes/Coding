7d3f7e209f96d911888ec959d001ea42c49f7c0c
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-13 16:43:39 +0530

    Create p1.c

---
+++ Prajjwal/day4/p1.c
@@ -0,0 +1,21 @@
+//day 4  1//
+
+#include <stdio.h>
+
+
+int main()
+{
+int a;
+
+
+printf("Enter number\t", a);
+scanf("%d", &a);
+
+if (a%2==0)
+printf("The number is even");
+
+else
+printf("The number is odd");
+
+    return 0;
+}
