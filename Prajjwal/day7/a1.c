0012a30b6d452f89a6d2f8028d80799e7dd66ce6
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-16 23:13:04 +0530

    Update a1.c

--- Prajjwal/day7/a1.c
+++ Prajjwal/day7/a1.c
@@ -6,9 +6,9 @@ int main()
 {
     int i,j;
 
-    for ( i = 1; i <=4; i++)
+    for ( i = 1; i <=4; i++) //line 1//
     {
-        for ( j= 1; j <= i; j++)
+        for ( j= 1; j <= i; j++)  //line 2//
         {
             printf("%d", j);
         }
