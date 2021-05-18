508edfbbe6113b3ceea7f82e5c849a4006c8dc0a
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-19 01:26:12 +0530

    Create st2.c

---
+++ Prajjwal/day9/st2.c
@@ -0,0 +1,23 @@
+// day 9  2//
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+
+for ( i = 9; i >= 5; i--)
+{
+
+   for (j=5; j<=i; j++)
+   {
+      printf("%d", j);
+   }
+   printf("\n");
+
+  
+}
+
+return 0;
+}
+
