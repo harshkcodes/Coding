f28c013a6b5c40aa11dc4279bf3278095fc865b5
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-27 00:41:27 +0530

    Create pro.c

---
+++ Prajjwal/day14/pro.c
@@ -0,0 +1,21 @@
+/*print the pattern :
+
+1
+2
+3
+4
+5
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    for ( i = 1; i <=5; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
