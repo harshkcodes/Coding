12428fd80641597cdb673aed5f1fd8b762c918bc
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-27 23:26:28 +0530

    Create soc.c

---
+++ Prajjwal/day15/soc.c
@@ -0,0 +1,17 @@
+/*pattern :
+
+A B C D E */
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char i;
+    for(i=65; i<=70; i++)
+    {
+        printf("%c ", i);
+    }
+    return 0;
+}
+
+
+ 
