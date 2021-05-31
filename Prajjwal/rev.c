83a2cb874502d36af6483e41312f8b2f31fdf46d
Prajjwal Goswami <77458269+prajjwal18@users.noreply.github.com>
2021-05-31 19:39:16 +0530

    Create rev.c

---
+++ Prajjwal/rev.c
@@ -0,0 +1,16 @@
+//day2 1//
+
+#include<stdio.h>
+
+
+int main()
+
+{
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(float));
+printf("%ld\n", sizeof(char));
+    printf("%ld\n", sizeof(double));
+
+
+    return 0;
+}
