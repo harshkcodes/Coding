bd2c98381407b95a37207c4316cde06e25ddd92f
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2021-11-28 17:01:47 +0530

    Create ghdg.c

---
+++ Prajjwal/ghdg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+
+
+    //int a[n];
+    int *a = (int *)malloc(sizeof(int) * n);
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum=sum+a[i];
+
+    }
+printf("%d",sum);
+    return 0;
+}
