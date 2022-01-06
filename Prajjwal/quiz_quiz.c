599cb703a7c67056393fe08c082cfdd4b71bd7bd
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2022-01-06 18:55:42 +0530

    Add files via upload

---
+++ Prajjwal/quiz_quiz.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i=0;
+    while(i<=20)
+{
+    if(i>=10){
+printf("The value of i is %d\n",i);
+    }
+    i++;
+}
+    return 0;
+}
